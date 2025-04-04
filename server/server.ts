ae2f26faa2b87a639fb3aa29dc1e86c9279c60a9
nikita_ba <n.bakunin@a1.by>
2025-04-04 20:45:32 +0300

    Базовая защита от повторного голоса

--- server/server.ts
+++ server/server.ts
@@ -6,16 +6,17 @@ interface Variant {
     id: number;
     text: string;
     votes: number;
-    imageUrl: string
+    imageUrl: string;
 }
+
 
 const app = express();
 const PORT = 7180;
 const DATA_FILE = path.resolve(__dirname, '../server/data.json');
+const USED_VOTES_FILE = path.resolve(__dirname, '../server/usedVotes.json');
 
 app.use(express.json());
 app.use('/public', express.static(path.resolve(__dirname, '../client/public')));
-
 
 const readData = (): Variant[] => {
     if (fs.existsSync(DATA_FILE)) {
@@ -34,20 +35,46 @@ const writeData = (variants: Variant[]) => {
     fs.writeFileSync(DATA_FILE, JSON.stringify(variants, null, 2));
 };
 
+const readUsedVoteIds = (): Set<string> => {
+    if (fs.existsSync(USED_VOTES_FILE)) {
+        const data = fs.readFileSync(USED_VOTES_FILE, 'utf-8');
+        return new Set(JSON.parse(data));
+    }
+    return new Set();
+};
+
+const writeUsedVoteIds = (usedVoteIds: Set<string>) => {
+    fs.writeFileSync(USED_VOTES_FILE, JSON.stringify(Array.from(usedVoteIds), null, 2));
+};
+
 let variants = readData();
+let usedVoteIds = readUsedVoteIds();
+
 app.get('/variants', (req: Request, res: Response) => {
     res.json(variants);
 });
 
 app.post('/vote', (req: Request, res: Response) => {
     const { id } = req.body;
+    const voteId = req.headers['x-vote-id'];
+
+
+    if (usedVoteIds.has(voteId as string)) {
+        res.json({ success: false, message: 'Ваш голос уже засчитан <3' });
+        return;
+    }
+
     const variant = variants.find(v => v.id === id);
     if (variant) {
         variant.votes += 1;
         writeData(variants);
+        usedVoteIds.add(voteId as string);
+        writeUsedVoteIds(usedVoteIds);
         res.json({ success: true });
+        return;
     } else {
         res.status(404).json({ success: false, message: 'Variant not found' });
+        return;
     }
 });
 
